91272de2faf5cb593cc1904d68d0a9203b4fc0f6
brkfstkng <97113790+brkfstkng@users.noreply.github.com>
2024-11-26 12:51:29 -0800

    Update custom-swipers.js

--- custom-swipers.js
+++ custom-swipers.js
@@ -36,3 +36,40 @@ const testimonialSwiper = new Swiper('.swiper.is-testimonial', {
     prevEl: '.swiper-t-prev',
     },
 });
+
+const groupSwiper = new Swiper('.swiper.is-photos', {
+    centeredSlides: true,
+    slidesPerView: 1.5,
+    spaceBetween: 16,
+    speed: 600,
+    loop: true,
+    autoplay: true,
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 32,
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-g-next',
+        prevEl: '.swiper-g-prev',
+        },
+});
+
+  // Fade up animation
+
+gsap.from("[data-gsap-fade='up']", {
+    scrollTrigger: {
+        trigger: "[data-gsap-fade='up']",
+        start: "top 80%", 
+        end: "bottom 20%",// Start animation when the top of each element reaches 80% of the viewport
+        toggleActions: "play none none none", // Play animation when in view
+    },
+    duration: 1,
+    y: 10,
+    opacity: 0,
+    stagger: 0.2,
+    ease: "power2.inOut",
+});
